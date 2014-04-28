71b98658bf6d3b262fc174568014d99f858bac26
Joshua Elson <josh@fluentstream.com>
2014-04-27 22:00:33 -0600

    Add relative pathing for debug on _api libraries

--- SIPml-api.js
+++ SIPml-api.js
@@ -9,7 +9,7 @@
 // ================== DEBUG CODE =============== //
 var __b_release_mode = false;
 var tag_hdr = document.getElementsByTagName('head')[0];
-['SIPml.js', 'src/tinySIP/src/tsip_api.js'].forEach(function (file) {
+['js/lib/sipml5/SIPml.js', 'js/lib/sipml5/src/tinySIP/src/tsip_api.js'].forEach(function (file) {
     var tag_script = document.createElement('script');
     tag_script.setAttribute('type', 'text/javascript');
     tag_script.setAttribute('src', file + "?svn=20");
